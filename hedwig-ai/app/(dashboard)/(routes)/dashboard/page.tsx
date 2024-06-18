d286f2c1628f6e2da47c8f999c7754ac90c7b332
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-06-18 15:07:46 +0600

    Folder Setup Done

---
+++ hedwig-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+const DashboardPage =()=> {
+  return (
+    <Button >Dashboard Page(Protected)</Button>
+  );
+}
+
+export default DashboardPage;
